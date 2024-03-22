71b105b3661511dfd40dd37d4a4071b50c639961
David_OH <erijesudo@gmail.com>
2024-03-22 05:43:37 +0100

    conn to 15m

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -43,8 +43,8 @@ func main() {
 	flag.StringVar(&cfg.Env, "env", "development", "Environment (development|staging|production)")
 	//db
 	flag.StringVar(&cfg.Db.Dsn, "db-dsn", dbUrl, "PostgreSQL DSN")
-	flag.IntVar(&cfg.Db.MaxOpenConns, "db-max-open-conns", 10, "PostgreSQL max open connections")
-	flag.IntVar(&cfg.Db.MaxIdleConns, "db-max-idle-conns", 5, "PostgreSQL max idle connections")
+	flag.IntVar(&cfg.Db.MaxOpenConns, "db-max-open-conns", 15, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.Db.MaxIdleConns, "db-max-idle-conns", 12, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.Db.MaxIdleTime, "db-max-idle-time", "1m", "PostgreSQL max connection idle time")
 	//limiters
 	flag.Float64Var(&cfg.Limiter.Rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
