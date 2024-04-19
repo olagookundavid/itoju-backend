548e7bd79f4df9b88a4ddb5f05a84c0fe6b04f56
David_OH <erijesudo@gmail.com>
2024-04-19 12:37:28 +0100

    added food status

--- internal/models/food_metric.go
+++ internal/models/food_metric.go
@@ -167,8 +167,8 @@ func (m FoodMetricModel) CheckUserEntry(userID string, date time.Time, sendbool 
 
 	query := `
 	SELECT COUNT(*) AS entry_count
-	FROM food_metrics fm
-	WHERE fm.user_id = $1 AND fm.date = $2
+	FROM user_food_metric ufm
+	WHERE ufm.user_id = $1 AND ufm.date = $2
 `
 	var entryCount int
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
